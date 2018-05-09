1415a95f39dad63a10bf10088db9f911dab9519e
suelliton <tonmelodicmetal@gmail.com>
2018-05-08 21:24:15 -0300

    adicao de arquivo taxas e versão atualizada

--- testecv2.py
+++ testecv2.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-nomeFoto = "alface2404 (3).jpg"
+nomeFoto = "imagens/alface0205 (2).jpg"
 img = cv2.imread(nomeFoto)
 
 r = img[:,:,2]
@@ -18,6 +18,13 @@ cv2.imwrite("blur.jpg",blur)
 blur = np.uint8(cv2.imread("blur.jpg"))
 
 
+height, width = blur.shape[:2]
+imResize = cv2.resize(blur,(412,310), interpolation = cv2.INTER_AREA)
+cv2.imwrite("imResize_.jpg",imResize)
+plt.imshow(imResize,'gray')
+plt.show()
+
+
 hist = cv2.calcHist([blur],[0],None,[256],[0,256])
 plt.plot(hist)
 plt.show()
@@ -27,11 +34,11 @@ plt.show()
 ##plt.imshow(imgLimiar,'gray')
 ##plt.show()
 
-imsaida = np.ones((len(img),len(img[0])),dtype=np.uint8)
+imsaida = np.ones((len(imResize),len(imResize[0])),dtype=np.uint8)
 cont = 0
-for i in range(0,len(img)):
-	for j in range(0,len(img[0])):
-		if blur[i][j][1] > 121 :
+for i in range(0,len(imResize)):
+	for j in range(0,len(imResize[0])):
+		if imResize[i][j][1] > 121 :
 			imsaida[i][j] = 255
 			cont +=1
 		else:
