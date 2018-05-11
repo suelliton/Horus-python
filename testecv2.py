a9e6b5280b2fb63b85dd92bd709bf4c91a6fa4c7
suelliton <tonmelodicmetal@gmail.com>
2018-05-10 21:18:55 -0300

    alterações em taxas.txt

--- testecv2.py
+++ testecv2.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-nomeFoto = "imagens/alface0205 (2).jpg"
+nomeFoto = "imagens/alface0205 (1).jpg"
 img = cv2.imread(nomeFoto)
 
 r = img[:,:,2]
@@ -16,29 +16,31 @@ blur = cv2.blur(imT,(3,3))
 blur = cv2.blur(blur,(3,3))
 cv2.imwrite("blur.jpg",blur)
 blur = np.uint8(cv2.imread("blur.jpg"))
+#blur = cv2.blur(blur,(5,5))
+#cv2.imwrite("blur2.jpg",blur)
+#blur = np.uint8(cv2.imread("blur2.jpg"))
+
+#height, width = imT.shape[:2]
+#imResize = cv2.resize(blur,(206,154), interpolation = cv2.INTER_AREA)
+#cv2.imwrite("imResize_.jpg",imResize)
+#plt.imshow(imResize,'gray')
+#plt.show()
 
 
-height, width = blur.shape[:2]
-imResize = cv2.resize(blur,(412,310), interpolation = cv2.INTER_AREA)
-cv2.imwrite("imResize_.jpg",imResize)
-plt.imshow(imResize,'gray')
-plt.show()
-
-
-hist = cv2.calcHist([blur],[0],None,[256],[0,256])
-plt.plot(hist)
-plt.show()
+#hist = cv2.calcHist([imT],[0],None,[256],[0,256])
+#plt.plot(hist)
+#plt.show()
 
 
 ##limiar, imgLimiar = cv2.threshold(g,127,255,cv2.THRESH_BINARY)
 ##plt.imshow(imgLimiar,'gray')
 ##plt.show()
 
-imsaida = np.ones((len(imResize),len(imResize[0])),dtype=np.uint8)
+imsaida = np.ones((len(imT),len(imT[0])),dtype=np.uint8)
 cont = 0
-for i in range(0,len(imResize)):
-	for j in range(0,len(imResize[0])):
-		if imResize[i][j][1] > 121 :
+for i in range(0,len(imT)):
+	for j in range(0,len(imT[0])):
+		if blur[i][j][1] > 121 :
 			imsaida[i][j] = 255
 			cont +=1
 		else:
