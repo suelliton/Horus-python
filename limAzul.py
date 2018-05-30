cfebd0a2f2f3597fbe980dc6ad2c992ff4bde747
suelliton <tonmelodicmetal@gmail.com>
2018-05-30 15:50:49 -0300

    inclusao do arquivo imAzul.py

---
+++ limAzul.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+nomeFoto = "azul2.jpg"
+img = cv2.imread(nomeFoto)
+imBlur = cv2.blur(img,(5,5))
+
+
+r = imBlur[:,:,2]
+g = imBlur[:,:,1]
+b = imBlur[:,:,0]
+
+imR = ((g) + (455-(b)))/4
+imT = (b) + (255 -r)
+imTemp = b - imR -r
+"""
+hist = cv2.calcHist([imTemp],[0],None,[256],[0,256])
+plt.plot(hist)
+plt.show()"""
+
+plt.imshow(imTemp,'gray')
+plt.show()
+plt.imshow(g,'gray')
+plt.show()
+plt.imshow(b,'gray')
+plt.show()
+
+
+
+
+
+##limiar, imgLimiar = cv2.threshold(g,127,255,cv2.THRESH_BINARY)
+##plt.imshow(imgLimiar,'gray')
+##plt.show()
+
+imsaida = np.ones((len(img),len(img[0])),dtype=np.uint8)
+cont = 0
+for i in range(0,len(img)):
+	for j in range(0,len(img[0])):
+		if imTemp[i][j] > 80 :
+			imsaida[i][j] = 255
+			cont +=1
+		else:
+			imsaida[i][j] = 0
+print("taxa:"+str(cont))
+print("tamanho : "+str(len(imsaida)))
+cv2.imwrite("saida.jpg",imsaida)
+plt.imshow(imsaida,'gray')
+plt.show()
